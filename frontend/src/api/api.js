4b16282487d283f9b6219b7e6324c3b39bf35da5
thavasi2003 <thavaseesw@gmail.com>
2025-12-12 18:48:17 +0530

    Updated project files ten

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // Connects to your local Node.js server
+  baseURL: 'http://3.6.69.48:5001/api', // Connects to your local Node.js server
 });
 
 // Request Interceptor: Attach Token to every request
